1878f9acb343646ed6481c8f8a0c32707597fb81
mmjvox <mmdjv.kh94@gmail.com>
2021-08-28 02:32:11 +0430

    adding tests

---
+++ test/MyRippleTest.pro
@@ -0,0 +1,11 @@
+QT += quick gui
+
+CONFIG += c++11
+
+include($$PWD/../AnotherRipple/AnotherRipple.pri)
+
+SOURCES += \
+        main.cpp
+
+RESOURCES += qml.qrc
+
---
+++ test/main.cpp
@@ -0,0 +1,28 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "../AnotherRipple/AnotherRipple.h"
+
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<AnotherRipple>("AnotherRipple", 1, 0, "SimpleRipple");
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
